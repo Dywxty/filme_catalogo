d245b49490e652e67e039bc871cad779897a5cd0
Dywxty <leticia.talachia@gmail.com>
2026-04-30 14:32:10 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 import psycopg
 
 def get_connection():
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = os.environ.get("postgresql://neondb_owner:npg_Qnv4i7XHwbUd@ep-rapid-truth-amjduagq-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
     if DATABASE_URL:
         return psycopg.connect(DATABASE_URL)
