860f57b772ae19bd658dd9d1aab4d411e7d8db33
Dywxty <leticia.talachia@gmail.com>
2026-04-30 10:30:50 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,6 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<project version="4">
-  <component name="VcsDirectoryMappings">
-    <mapping directory="$PROJECT_DIR$" vcs="Git" />
-  </component>
-</project>
+import psycopg2
+import os
+
+def get_connection():
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if DATABASE_URL:
+        return psycopg2.connect(DATABASE_URL)
+
+    return psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="1234",
+        database="catalogo_filmes"
+    )
